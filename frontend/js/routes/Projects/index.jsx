8996b0233abe73cb08cb8824b8f55aca92ad77a2
Kalob Porter <kalob@kalob.net>
2020-07-03 14:50:32 -0400

    Updates to include wedding app & info in html comment

--- frontend/js/routes/Projects/index.jsx
+++ frontend/js/routes/Projects/index.jsx
@@ -8,88 +8,7 @@ import { makeStyles } from '@material-ui/styles';
 
 import Project from './Project';
 import styles from './style';
-
-const projectData = [
-  {
-    name: 'Guardian News JS Client',
-    desc: (
-      <>
-        A JavaScript client that I wrote to go along with the
-        {' '}
-        <a href="http://open-platform.theguardian.com/documentation/">Guardian Open Platform</a>
-      </>
-    ),
-    link: {
-      type: 'github',
-      url: 'https://github.com/PorterK/GuardianJSClient',
-    },
-    tech: [
-      'JS',
-    ],
-  },
-  {
-    name: 'Robovise',
-    desc: (
-      <>
-        A robo advisor built for the modern world.
-      </>
-    ),
-    link: {
-      type: 'site',
-      url: 'https://robovise.com',
-    },
-    tech: [
-      'JS', 'React', 'Webpack', 'AWS',
-    ],
-  },
-  {
-    name: 'DQO Index Calculator',
-    desc: (
-      <>
-        A calculator to determine the isocyanate index and theoretical isocyanurate of
-        specified ingredients.
-      </>
-    ),
-    link: {
-      type: 'site',
-      url: 'https://www.dqoconsulting.com/new-page',
-    },
-    tech: [
-      'JS', 'React', 'Heroku',
-    ],
-  },
-  {
-    name: 'Sump Thing',
-    desc: (
-      <>
-        An open source aquarium monitoring hardware/software solution.
-      </>
-    ),
-    link: {
-      type: 'github',
-      url: 'https://github.com/pyazo',
-    },
-    tech: [
-      'C++', 'C#', 'JS', 'React', 'Arduino',
-    ],
-  },
-  {
-    name: 'Uncovered',
-    desc: (
-      <>
-        A platform to use the power of collective impact to bring peace to
-        families of murdered or missing people.
-      </>
-    ),
-    link: {
-      type: 'site',
-      url: 'https://cases.uncovered.com',
-    },
-    tech: [
-      'JS', 'React',
-    ],
-  },
-];
+import data from './projects-data';
 
 const useStyles = makeStyles(styles);
 
@@ -118,7 +37,7 @@ export default function Projects() {
         </Grid>
       </Grid>
       {
-        projectData.map((p, index) => <Project p={p} index={index} key={p.name} />)
+        data.map((p, index) => <Project p={p} index={index} key={p.name} />)
       }
       <Grid item xs={12} sm={8}>
         <Grid container justify="space-between">
---
+++ frontend/js/routes/Projects/projects-data.js
@@ -0,0 +1,100 @@
+import React from 'react';
+
+const data = [
+  {
+    name: 'Guardian News JS Client',
+    desc: (
+      <>
+        A JavaScript client that I wrote to go along with the
+        {' '}
+        <a href="http://open-platform.theguardian.com/documentation/">Guardian Open Platform</a>
+      </>
+    ),
+    link: {
+      type: 'github',
+      url: 'https://github.com/PorterK/GuardianJSClient',
+    },
+    tech: [
+      'JS',
+    ],
+  },
+  {
+    name: 'Robovise',
+    desc: (
+      <>
+        A robo advisor built for the modern world.
+      </>
+    ),
+    link: {
+      type: 'site',
+      url: 'https://robovise.com',
+    },
+    tech: [
+      'JS', 'React', 'Webpack', 'AWS', 'Node', 'ECS',
+    ],
+  },
+  {
+    name: 'DQO Index Calculator',
+    desc: (
+      <>
+        A calculator to determine the isocyanate index and theoretical isocyanurate of
+        specified ingredients.
+      </>
+    ),
+    link: {
+      type: 'site',
+      url: 'https://www.dqoconsulting.com/new-page',
+    },
+    tech: [
+      'JS', 'React', 'Heroku', 'Node',
+    ],
+  },
+  {
+    name: 'Sump Thing',
+    desc: (
+      <>
+        An open source aquarium monitoring hardware/software solution.
+      </>
+    ),
+    link: {
+      type: 'github',
+      url: 'https://github.com/pyazo',
+    },
+    tech: [
+      'C++', 'C#', 'JS', 'React', 'Arduino', 'Node',
+    ],
+  },
+  {
+    name: 'Uncovered',
+    desc: (
+      <>
+        A platform to use the power of collective impact to bring peace to
+        families of murdered or missing people.
+      </>
+    ),
+    link: {
+      type: 'site',
+      url: 'https://cases.uncovered.com',
+    },
+    tech: [
+      'JS', 'React', 'Node', 'Heroku',
+    ],
+  },
+  {
+    name: 'Wedding App',
+    desc: (
+      <>
+        An app to assist in tracking RSVPs and mass communication for my wedding.
+      </>
+    ),
+    link: {
+      type: 'site',
+      url: 'https://porter.family',
+    },
+    tech: [
+      'JS', 'Ember', 'Firebbase', 'S3',
+    ],
+  },
+];
+
+export default data;
